446fb03292438d44733e91ec6cbc04e067a71328
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2015-09-30 16:04:35 +0100

    Allow ensure => absent for versions.
    In order to allow removing a version from a machine, this adds the
    abiliry to specify `ensure => absent` for a version.

--- manifests/version.pp
+++ manifests/version.pp
@@ -8,15 +8,36 @@
 #
 # goenv { ['1.2.2', '1.3.1']: }
 #
-define goenv::version () {
+# === Parameters
+#
+# [*ensure*]
+#   present or absent. Whether to install or remove this version.
+#   Default: present
+#
+define goenv::version (
+  $ensure = present,
+) {
   include goenv::params
 
   $version = $title
 
-  exec { "install-goenv-go-${version}":
-    command     => "${goenv::params::goenv_binary} install ${version}",
-    environment => "GOENV_ROOT=${goenv::params::goenv_root}",
-    creates     => "${goenv::params::goenv_root}/versions/${version}/bin/go",
-    require     => Class['goenv'],
+  if $ensure == present {
+
+    exec { "install-goenv-go-${version}":
+      command     => "${goenv::params::goenv_binary} install ${version}",
+      environment => "GOENV_ROOT=${goenv::params::goenv_root}",
+      creates     => "${goenv::params::goenv_root}/versions/${version}/bin/go",
+      require     => Class['goenv'],
+    }
+
+  } else {
+
+    exec { "uninstall-goenv-go-${version}":
+      command     => "${goenv::params::goenv_binary} uninstall ${version}",
+      environment => "GOENV_ROOT=${goenv::params::goenv_root}",
+      onlyif      => "test -d ${goenv::params::goenv_root}/versions/${version}",
+      require     => Class['goenv'],
+    }
+
   }
 }
